dacdb8f59c204341c6ee64c257ac52ff6ff5294f
eswar.vandanapu1 <eswar.vandanapu@gmail.com>
2019-12-22 14:52:12 -0800

    Required changes for making sure non python files are included

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="sphinxcontrib-drawio-html",
-    version="0.1",
+    version="0.1.2",
     author="Eswar Vandanapu",
     author_email="eswar.vandanapu@gmail.com",
     description="Sphinx Extension to include draw.io files using HTML5 embedding",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/gotpredictions/sphinxcontrib-drawio-html",
-    packages=['sphinxcontrib'],
+    packages=['sphinxdrawio'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
@@ -20,4 +20,5 @@ setuptools.setup(
         "Framework :: Sphinx :: Extension",
     ],
     python_requires='>=3.7',
+    include_package_data=True
 )
